0bfa78bbf968f1fbcab925175158280c05edd6aa
Rigney <krigney@proforma.com>
2018-04-16 14:51:24 -0400

    Add project files.

---
+++ IntroToEF-Transactions/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroToEF_Transactions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
